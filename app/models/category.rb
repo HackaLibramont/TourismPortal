71ee551f913fd9d423273c7c95c178ccda208e6b
cedric <cedric@studio-sept.be>
2015-03-21 16:33:45 +0100

    Skip TEC interests from InterestFinder

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,10 @@ class Category < ActiveRecord::Base
   has_many :children, class_name: Category
   scope :root, -> { where(category_id: nil) }
 
+  def self.tec_id
+    10
+  end
+
   def has_children?
     children.any?
   end
