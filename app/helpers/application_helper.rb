eb6724f31f28be4e7de3b70f217d58d4986982bd
cedric <cedric@studio-sept.be>
2015-03-21 16:57:05 +0100

    Add icon to map

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,26 @@ module ApplicationHelper
       content_tag(:h2, title, class: 'page-title')
     end
   end
+
+  def icon_url_for(category_label)
+    # category_label.downcase!
+    case category_label.parameterize
+    when "hébergement"
+      "/assets/fontawesome/building8.png"
+    when "terroir-gastronomie"
+      "/assets/fontawesome/fork9.png"
+    when "loisir-attraction"
+      "/assets/fontawesome/dribbble4.png"
+    when "culture"
+      "/assets/fontawesome/book95.png"
+    when "balade-randonnee"
+      "/assets/fontawesome/plant16.png"
+    when "divertissement"
+      "/assets/fontawesome/film28.png"
+    when "evenement"
+      "/assets/fontawesome/calendar52.png"
+    when "transport"
+      "/assets/fontawesome/road16.png"
+    end
+  end
 end
