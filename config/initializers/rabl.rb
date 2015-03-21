1994eea4663b1999e711d3bdd2161905661b4d21
cedric <cedric@studio-sept.be>
2015-03-21 16:00:44 +0100

    Change cache strategy

--- config/initializers/rabl.rb
+++ config/initializers/rabl.rb
@@ -1,7 +1,7 @@
 require 'rabl'
 Rabl.configure do |config|
   # Commented as these are defaults
-  # config.cache_all_output = false
+  config.cache_all_output = true
   config.cache_sources = Rails.env.production? # Defaults to false
   # config.cache_engine = Rabl::CacheEngine.new # Defaults to Rails cache
   # config.perform_caching = false
